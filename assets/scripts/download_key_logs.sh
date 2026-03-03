ba8220f3eac07bd17578455a0c2b76155890f628
Prashant Kalvapalle <ppresh.ant@gmail.com>
2026-03-03 14:55:15 -0600

    (_untested_) auto-extract of latest log timestamp

--- assets/scripts/download_key_logs.sh
+++ assets/scripts/download_key_logs.sh
@@ -5,6 +5,14 @@
 # get the timestamp of the run
 RUN_TIMESTAMP=$1
 
+# if timestand is not provided, parse the latest run timestamp from the pipeline_info directory
+# note : the last run file with be in the format of execution_timeline_2026-03-03_13-23-13.html
+# note : the format of the timestamp is YYYY-MM-DD_HH-MM-SS
+if [ -z "$RUN_TIMESTAMP" ]; then
+    RUN_TIMESTAMP=$(ls -td ${BASE_DIR}/results/pipeline_info/* | head -n 1 | cut -d '_' -f 4-5 | cut -d '.' -f 1)
+    echo "No timestamp provided. Extracted the latest run timestamp: $RUN_TIMESTAMP"
+fi
+
 # Set the base directory
 BASE_DIR="/home/Users/pbk1/Somatem"
 
